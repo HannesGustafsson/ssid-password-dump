60b2383f4a96d88fe2636ae3bd5521bfc36ba72e
Hannes Gustafsson <hannesgustafsson@hotmail.com>
2021-08-03 13:51:24 +0200

    Create PW_DUMP.ps1

---
+++ PW_DUMP.ps1
@@ -0,0 +1,9 @@
+﻿(netsh wlan show profiles) |
+Select-String "\:(.+)$" |
+%{$name=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{(netsh wlan show profile name="$name" key=clear)}  |
+Select-String "Key Content\W+\:(.+)$" |
+%{$pass=$_.Matches.Groups[1].Value.Trim(); $_} |
+%{[PSCustomObject]@{ SSID_NAME=$name;PASSWORD=$pass }} |
+Format-Table -AutoSize |
+Out-File -Append -FilePath .\PW_DUMP.txt
